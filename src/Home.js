ab7390a45d74f8289e4a68cf749d6cf5088c5d27
Vickee <Vignesrajesh02@gmail.com>
2024-07-29 12:47:05 +0530

    Front end files modified

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ function Home() {
     <div>
       <nav class="navbar navbar-expand-lg bg-light fixed-top" >
         <div className='container-fluid'>
-            <a href='' className='navbar-brand'>
-                <img src='https://t4.ftcdn.net/jpg/04/06/91/91/360_F_406919161_J0pGxe1sewqnk5dnvyRS77MKmEd6SVac.jpg' class="logo"></img>
+            <a className='navbar-brand'>
+                <img src='https://t4.ftcdn.net/jpg/04/06/91/91/360_F_406919161_J0pGxe1sewqnk5dnvyRS77MKmEd6SVac.jpg' class="logo" alt=''></img>
                 <h5>MeeVickee</h5>
             </a>
         </div>
