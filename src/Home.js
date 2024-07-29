ebd6d846065685403478620db40e8066c4465695
Vickee <Vignesrajesh02@gmail.com>
2024-07-29 12:51:09 +0530

    Front end files modified

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,10 @@ function Home() {
     <div>
       <nav class="navbar navbar-expand-lg bg-light fixed-top" >
         <div className='container-fluid'>
-            <a className='navbar-brand'>
+            <button className='navbar-brand'>
                 <img src='https://t4.ftcdn.net/jpg/04/06/91/91/360_F_406919161_J0pGxe1sewqnk5dnvyRS77MKmEd6SVac.jpg' class="logo" alt=''></img>
                 <h5>MeeVickee</h5>
-            </a>
+            </button>
         </div>
         <div className='conatiner-fluid' id='div2'>
         <ul class="navbar-nav">
